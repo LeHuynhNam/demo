42397ca11cb361fbc563139de9bb8fc6923a754f
HuynhNam <lehuynhnam2001@gmail.com>
2022-03-24 09:03:38 +0700

    PTPMBuoi1

---
+++ LeHuynhNam_Buoi1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeHuynhNam_Buoi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+         
+
+
+            //check box
+            //            string[] foods ={"Grain", "Bread", "Beans", "Eggs", "Chicken", "Milk",
+            //"Fruit", "Vegetables", "Pasta", "Rice", "Fish", "Beef" };
+            //            int topPosition = 10;
+            //            foreach (string food in foods)
+            //            {
+            //                CheckBox cb = new CheckBox();
+            //                cb.Left = 30;
+            //                cb.Top = topPosition;
+            //                topPosition += 30;
+            //                cb.Text = food;
+            //                Controls.Add(cb);
+
+            //            }
+
+            //text box
+            string[] textBox = { "textBox1", "textBox2", "textBox3", "textBox4" };
+            int topPosition = 50;
+            foreach (string textB in textBox)
+            {
+                TextBox tb = new TextBox();
+                tb.Left = 20;
+                tb.Top = topPosition;
+                tb.Size = new Size(170, 100);
+                               topPosition += 50;
+                              tb.Text = textB;
+                              Controls.Add(tb);
+            
+
+                Controls.Add(tb);
+            }
+            //buton
+            Button b = new Button();
+            b.Text = "Process Text Boxes";
+            b.Size=new Size(150, 20);
+            b.Left = 20;
+            b.Top = 240;
+            this.Controls.Add(b);
+            b.Click += new System.EventHandler(bt_click);
+        }
+        private void bt_click(object sender, EventArgs e)
+        {
+            //ham clear 
+            foreach (Control item in Controls)
+            {
+                if (item.GetType() == typeof(TextBox))
+                {
+                    item.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ LeHuynhNam_Buoi1/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeHuynhNam_Buoi1
+{
+    public partial class Form2 : Form
+    {
+        Panel pn = new Panel();
+        
+
+        public Form2()
+        {
+            InitializeComponent();
+             int dong = 10;
+            //
+
+        int cot =10;
+        int topPosition = 10;
+     
+        for(int i=0; i< dong; i++)
+        {
+            int LeftPosition = 10;
+            for( int j=0; j<cot;j++)
+            {
+                 Button b = new Button();
+          
+            b.Size=new Size(30, 30);
+            b.Left = 20;   
+            b.Left=LeftPosition;
+            LeftPosition +=30;
+            b.Top = topPosition;
+            this.Controls.Add(b);
+            b.Click += new System.EventHandler(bt_click);
+            }
+            topPosition += 30;
+        }
+
+
+        }
+
+private void bt_click(object sender, EventArgs e)
+{
+ 	
+}
+       
+    
+
+    }
+    }
+
